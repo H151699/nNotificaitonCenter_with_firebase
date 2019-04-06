1dabd2b2df1f312bff48c55fe1d8c03a079db194
h151699 <h151699@stud.hvl.no>
2019-04-06 11:52:07 +0200

    add signin form, and create its route /signin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectsDetails from './components/projects/ProjectsDetails'
+import signIn from './components/auth/signIn';
 
 class App extends Component {
   render() {
@@ -13,8 +14,9 @@ class App extends Component {
       <div className="App">
         <Navbar/>
         <Switch> 
-          <Route exact path= '/' component={Dashboard} /> 
+          <Route exact path= '/'component={Dashboard} /> 
           <Route path = '/project/:id' component ={ProjectsDetails}/>
+          <Route path = '/signin' component ={signIn}/>
         </Switch>
 
       </div>
