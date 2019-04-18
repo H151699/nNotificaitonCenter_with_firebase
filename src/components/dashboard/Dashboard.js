38f0f528c7c9ae42ba6b5b4ca23ce3f106de40b4
h151699 <h151699@stud.hvl.no>
2019-04-18 23:39:29 +0200

    connect Dashboard component with redux store

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,18 +2,23 @@ import React, {Component} from 'react'
 import Notifications from './Notifications'
 
 import ProjectList from '../projects/ProjectList'
+import {connect} from 'react-redux'
+// react-redux : a glue library, that glues redux to react
+// here connect Dashboard component with redux store
 
 
 
 class Dashboard extends Component{
     render(){
+        //console.log(this.props);
+        const {projects} = this.props;
         return (
             <diV className="dashboard container">
 
                 <div className="row">
                     
                     <div className="col s12 m6"> 
-                        <ProjectList />
+                        <ProjectList projects ={projects}/>
                     </div> 
                         
 
@@ -28,4 +33,18 @@ class Dashboard extends Component{
 
 }
 
-export default Dashboard
+
+
+// map the state from the store to the props in this componet
+// takes the state of the store, 
+
+const mapStateToProps = (state) => {
+    return {
+        // return an object that represent which properties are attached to the props of this compent
+        // then access them inside the componet
+        // objects: state. project(root).property in the projectReducer
+        projects: state.project.projects
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
