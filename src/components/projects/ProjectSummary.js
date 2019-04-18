66e74663e79eecb1b037a194273c8ecab2a96cf4
h151699 <h151699@stud.hvl.no>
2019-04-18 23:40:19 +0200

    access project property from reducer store

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return(
 
         <div className="card z-depth-0 project-summary">
             <div className ="card-content grey-text text-darken-3">
-                <span className="card-tittle"> Project tittle</span>
+                <span className="card-tittle"> {project.title}}</span>
                 <p>Posted by Andrew Jianyou Day </p>
                 <p className="grey-text">3rd September, 2am</p>
             </div>
