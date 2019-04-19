196c932530050dbed1a32896edea71f2680cf5fc
h151699 <h151699@stud.hvl.no>
2019-04-19 21:26:03 +0200

    set up firebase firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,7 @@
 
 export const createProject =(project) => {
     // action
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
      // make async call  to database
      dispatch({type: 'CREATE_PROJECT',  project});  // dispatch the action
     }
